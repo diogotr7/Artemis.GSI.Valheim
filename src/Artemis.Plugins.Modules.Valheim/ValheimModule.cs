92b0d18e948322f6b734ceb6c6058d5a4ef16457
Diogo Trindade <diogotr7@gmail.com>
2021-06-10 16:14:09 +0100

    Updated for Artemis changes

--- src/Artemis.Plugins.Modules.Valheim/ValheimModule.cs
+++ src/Artemis.Plugins.Modules.Valheim/ValheimModule.cs
@@ -1,13 +1,23 @@
-﻿using Artemis.Core.Modules;
+﻿using Artemis.Core;
+using Artemis.Core.Modules;
 using Artemis.Core.Services;
 using Artemis.Plugins.Modules.Valheim.DataModels;
-using SkiaSharp;
+using System.Collections.Generic;
 
 namespace Artemis.Plugins.Modules.Valheim
 {
-    public class ValheimModule : ProfileModule<ValheimDataModel>
+    [PluginFeature(AlwaysEnabled = true, Name = "Valheim", Icon = "Valheim.svg")]
+    public class ValheimModule : Module<ValheimDataModel>
     {
         private readonly IWebServerService _webServerService;
+
+        private PluginEndPoint _playerUpdateEndpoint;
+        private PluginEndPoint _environmentUpdateEndpoint;
+        private PluginEndPoint _teleportEventEndpoint;
+        private PluginEndPoint _levelUpEventEndpoint;
+        private PluginEndPoint _lforsakenEventEventEndpoint;
+
+        public override List<IModuleActivationRequirement> ActivationRequirements { get; } = new() { new ProcessActivationRequirement("valheim") };
 
         public ValheimModule(IWebServerService webServerService)
         {
@@ -16,35 +26,20 @@ namespace Artemis.Plugins.Modules.Valheim
 
         public override void Enable()
         {
-            DisplayName = "Valheim";
-            DisplayIcon = "Valheim.svg";
-            DefaultPriorityCategory = ModulePriorityCategory.Application;
-            ActivationRequirements.Add(new ProcessActivationRequirement("valheim"));
-
-            _webServerService.AddJsonEndPoint<PlayerData>(this, "player", p =>
-            {
-                DataModel.Player = p;
-            });
-            _webServerService.AddJsonEndPoint<Enviroment>(this, "environment", e =>
-            {
-                DataModel.Environment = e;
-            });
-            _webServerService.AddStringEndPoint(this, "teleport", _ =>
-            {
-                DataModel.Teleport.Trigger();
-            });
-            _webServerService.AddJsonEndPoint<SkillLevelUpEventArgs>(this, "levelUp", e =>
-            {
-                DataModel.SkillLevelUp.Trigger(e);
-            });
-            _webServerService.AddStringEndPoint(this, "forsakenActivated", _ =>
-            {
-                DataModel.ForsakenActivated.Trigger();
-            });
+            _playerUpdateEndpoint = _webServerService.AddJsonEndPoint<PlayerData>(this, "player", p => DataModel.Player = p);
+            _environmentUpdateEndpoint = _webServerService.AddJsonEndPoint<Enviroment>(this, "environment", e => DataModel.Environment = e);
+            _teleportEventEndpoint = _webServerService.AddStringEndPoint(this, "teleport", _ => DataModel.Teleport.Trigger());
+            _levelUpEventEndpoint = _webServerService.AddJsonEndPoint<SkillLevelUpEventArgs>(this, "levelUp", e => DataModel.SkillLevelUp.Trigger(e));
+            _lforsakenEventEventEndpoint = _webServerService.AddStringEndPoint(this, "forsakenActivated", _ => DataModel.ForsakenActivated.Trigger());
         }
 
         public override void Disable()
         {
+            _webServerService.RemovePluginEndPoint(_playerUpdateEndpoint);
+            _webServerService.RemovePluginEndPoint(_environmentUpdateEndpoint);
+            _webServerService.RemovePluginEndPoint(_teleportEventEndpoint);
+            _webServerService.RemovePluginEndPoint(_levelUpEventEndpoint);
+            _webServerService.RemovePluginEndPoint(_lforsakenEventEventEndpoint);
         }
 
         public override void ModuleActivated(bool isOverride)
@@ -56,10 +51,6 @@ namespace Artemis.Plugins.Modules.Valheim
         }
 
         public override void Update(double deltaTime)
-        {
-        }
-
-        public override void Render(double deltaTime, SKCanvas canvas, SKImageInfo canvasInfo)
         {
         }
     }
